42e36b36bfa151a13cc7b6eb471f7aa3c36be231
leandro nicolas ovejero <leandroovejero16197@gmail.com>
2022-05-18 15:23:48 -0300

    inicio de aplicacion

---
+++ Sprint/route.php
@@ -0,0 +1,16 @@
+<?php
+
+define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+
+
+if(!empty($_GET["action"])){
+    $action = $_GET["action"];
+}else{
+    $action='home';
+}
+
+switch($params[0]){
+ 
+}
+
+?>
