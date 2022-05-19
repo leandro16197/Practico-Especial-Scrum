241d3953221bc7fce8b213aeae14c89ff6df7b12
Caseres Marina <marinacaseres1997@gmail.com>
2022-05-19 12:55:48 -0300

    Arreglados conflictos de merge.

--- Sprint/Controller/TurnosController.php
+++ Sprint/Controller/TurnosController.php
@@ -1,38 +1,26 @@
 <?php
 require_once ('./Helper/AuthHelper.php');
-<<<<<<< HEAD
 require_once('./Model/TurnModel.php');
-=======
-require_once('./Model/TurnosModel.php');
->>>>>>> TM-29
 require_once('./View/TurnosView.php');
 
 class TurnosController{
     private $model;
     private $view;
+
     function __construct(){
-<<<<<<< HEAD
       $this->model=new TurnModel();
-=======
-      $this->model=new TurnosModel();
->>>>>>> TM-29
       $this->view=new TurnosView();
     }
 
     function getHome(){
-<<<<<<< HEAD
         $Turno=$this->model->getTurnsByPatientId(11223344);
         $this->view->showTurnos($Turno);
     }
-    
-=======
-        $Turno=$this->model->getTurno();
-        $this->view->showTurnos($Turno);
-    }
-    
+
+
+
     function createTurno(){
       $this->view->createTurno();
     }
 
->>>>>>> TM-29
 }
