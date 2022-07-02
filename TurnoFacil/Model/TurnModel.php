618f4e504270790668757a4b723c9ebaa33bcfe4
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2022-07-02 12:53:01 -0300

    Terminada funcionalidad de eliminar turno de medico en Emergencia

--- TurnoFacil/Model/TurnModel.php
+++ TurnoFacil/Model/TurnModel.php
@@ -78,4 +78,9 @@ class TurnModel
         $query = $this->db->prepare($queryString);
         $query->execute(array(1, $id));
     }
+    function deleteTurn($id){
+        $queryString = "DELETE FROM turno WHERE id_turno = ?";
+        $query = $this->db->prepare($queryString);
+        $query->execute(array($id));
+    }
 }
